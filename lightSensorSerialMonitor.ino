e30a2077990e555e6da7e2520b11827b8cfab2ad
ARTTClub <38737943+ARTTClub@users.noreply.github.com>
2018-05-09 18:24:29 -0400

    Formatting spacing

--- lightSensorSerialMonitor.ino
+++ lightSensorSerialMonitor.ino
@@ -1,4 +1,6 @@
-int lightsensorPin=A0; int ledpin=9; void setup() {
+int lightsensorPin=A0; int ledpin=9; 
+
+void setup() {
 
 pinMode(lightsensorPin,INPUT); pinMode(ledpin, OUTPUT); Serial.begin(9600);
 
@@ -10,4 +12,13 @@ int lightsensorStatus=analogRead(lightsensorPin);
 
 Serial.println(lightsensorStatus);
 
-if(lightsensorStatus <=700) { digitalWrite(ledpin, HIGH); Serial.println("Room is Dark, LED is ON"); } else { digitalWrite(ledpin, LOW); Serial.println("Room is Bright, LED is OFF"); } }
+if(lightsensorStatus <=700) 
+  { 
+    digitalWrite(ledpin, HIGH); 
+    Serial.println("Room is Dark, LED is ON");  
+  } else { 
+    digitalWrite(ledpin, LOW); 
+    Serial.println("Room is Bright, LED is OFF"); 
+  } 
+
+}
