47f91d2d8368755b7d858f6fc22e074366dccca0
ARTTClub <38737943+ARTTClub@users.noreply.github.com>
2018-05-09 18:22:38 -0400

    Create lightSensorSerialMonitor.ino

---
+++ lightSensorSerialMonitor.ino
@@ -0,0 +1,13 @@
+int lightsensorPin=A0; int ledpin=9; void setup() {
+
+pinMode(lightsensorPin,INPUT); pinMode(ledpin, OUTPUT); Serial.begin(9600);
+
+}
+
+void loop() {
+
+int lightsensorStatus=analogRead(lightsensorPin);
+
+Serial.println(lightsensorStatus);
+
+if(lightsensorStatus <=700) { digitalWrite(ledpin, HIGH); Serial.println("Room is Dark, LED is ON"); } else { digitalWrite(ledpin, LOW); Serial.println("Room is Bright, LED is OFF"); } }
