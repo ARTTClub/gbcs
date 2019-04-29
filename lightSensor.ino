c98b990e30204fea05c0306bd68dc258e14ef0f5
mehul0sejal <mehul_sejal@yahoo.com>
2019-04-29 16:47:25 -0500

    updated code

--- lightSensor.ino
+++ lightSensor.ino
@@ -1,18 +1,25 @@
-int lightsensorPin = A0; int ledPin = 9;
 
-void setup() { 
-  Serial.begin(9600); 
-  pinMode(lightsensorPin, INPUT); 
-  pinMode(ledPin, OUTPUT);
-}
+ int lightsensorPin = A0; 
+ int ledPin = 9;
 
-void loop() { 
-int lightsensorStatus = analogRead(lightsensorPin); 
-if (lightsensorStatus >=1065) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("Room is Dark, LED is ON");
-  } else { 
-    digitalWrite(ledPin, LOW);
-    Serial.println("Room is lit, LED is OFF"); 
-  } 
+// int lightsensorStatus;
+ void setup() {
+ Serial.begin(9600);
+ pinMode(lightsensorPin, INPUT);
+ pinMode(ledPin, OUTPUT);
+ }
+
+void loop() {
+  int lightsensorStatus = analogRead(lightsensorPin);
+   Serial.println(lightsensorStatus);
+
+  if (lightsensorStatus <= 700) {
+  digitalWrite(ledPin, HIGH);
+  Serial.println("Room is dark, Led is ON");
+  }  else {
+  digitalWrite(ledPin, LOW);
+  Serial.println("Room is lit, Led is OFF");
+  }
+
+  delay(1000);
 }
